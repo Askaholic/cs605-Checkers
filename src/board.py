81c3c546b4904e81d35e8f03d6c436e31c024b3d
Jacob McKenna <mcjacob61@gmail.com>
2018-01-22 09:09:57 -0900

    commented and partial move table

--- src/board.py
+++ src/board.py
@@ -7,8 +7,9 @@ class Board(object):
 	def __init__(self):
 		
 		self.board = ['1']*32
+		self.playerTurn = 0 
 
-
+	# Sets up the pieces. 
 	def setupBoard(self):
 
 		for checker in range(12):
@@ -17,6 +18,39 @@ class Board(object):
 
 	def printRawBoard(self):
 		print(self.board)
+
+
+	def buildMoveTable(self):
+		self.moveTable = 
+		{
+			0: [4,5],
+			1: [5,6],
+			2: [6,7],
+			3: [7]
+
+			4: [0,8],
+			5: [0,1,8,9],
+			6: [1,2,9,10],
+			7: [2,3,10,11],
+
+			8: [4,5,12,13],
+			9: [5,6,13,14],
+			10: [6,7,14,15],
+			11: [7,15],
+
+			12: [8,16],
+			13: [8,9,16,17],
+			14: [9,10,17,18],
+			15: [10,11,18,19],
+
+			16: [12,13,20,21],
+			17: [13,14,21,22],
+			18: [14,15,22,23],
+			19: [15,23]
+
+
+
+		}
 
 
 	def printBoard(self):
--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3.6
 
+# Jacob McKenna
+# UAF CS605 AI
+# Created 1/19/18
+
+# Board class and object
 import board
 
+# Create board object
 b = board.Board()
 
+# Visual testing
 b.setupBoard()
 b.printBoard()
