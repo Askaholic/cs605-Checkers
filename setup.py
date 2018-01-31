8109e84f2e412595a9611aa3a5219b5916f30604
Askaholic <askaholic907@gmail.com>
2018-01-31 12:43:25 -0900

    Moved c++ files to their own directory

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup, Extension
 
 extension_mod = Extension('board_funcs',
                           sources=[
-                            'board_funcs_module.cpp',
-                            'board_funcs.cpp'],
+                            'src/board_funcs/board_funcs_module.cpp',
+                            'src/board_funcs/board_funcs.cpp'],
                           extra_compile_args=['-std=c++14'])
 
 setup(name='board_funcs', ext_modules=[extension_mod])
