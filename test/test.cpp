bffb2e4dd539f54a5ec4195bf152fd4c1ce6fa3f
Dope Beats <reweeden@alaska.edu>
2018-02-21 21:55:44 -0900

    Started unit test file

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+// test.cpp
+// Rohan Weeden
+// Created: Feb. 21, 2018
+
+// Unit tests using the catch.hpp framework. Make sure you have catch.hpp in
+// your path when compiling this file
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../src/board_funcs/board_funcs.h"
+
+TEST_CASE( "Generate Jumps", "[jump gen]") {
+
+}
