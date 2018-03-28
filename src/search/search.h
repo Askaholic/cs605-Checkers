437584bbe8a3c1c5dd168e49d8cfb63d4f1acd0c
Dope Beats <reweeden@alaska.edu>
2018-03-28 00:02:51 -0800

    Removed old search functions.

--- src/search/search.h
+++ src/search/search.h
@@ -11,15 +11,17 @@
 
 #include "board_state.h"
 #include <functional>
-#include <memory>
 
 
 #define BRANCH_FACTOR 32
 
 
-std::pair<BoardState, float> min_max_search_inplace(const BoardState & board, int player, int depth, std::function<float(const BoardState &, int)>);
-std::pair<BoardState, int> min_max_search_ab(const BoardState & board, int player, int depth);
-std::pair<std::unique_ptr<BoardState>, int> min_max_search(const BoardState & board, int player, int depth);
+std::pair<BoardState, float> min_max_search_inplace(
+    const BoardState & board,
+    int player,
+    int depth,
+    std::function<float(const BoardState &, int)>
+);
 
 
 #endif
