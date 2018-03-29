d2b7419174ff4af686b73b76687e741e7e3ae001
Askaholic <askaholic907@gmail.com>
2018-03-28 16:32:27 -0800

    Moved player objects into their own cpp

--- makefile
+++ makefile
@@ -19,6 +19,7 @@ SOURCE_FILES=\
 	src/board/moves/jump_table.cpp \
 	src/board/moves/jump_generator.cpp \
 	src/game/game.cpp \
+	src/game/player.cpp \
 	src/board_funcs/helpers.cpp \
 	src/search/little_search.cpp \
 	src/asknet/asknet.cpp \
@@ -28,9 +29,11 @@ MAIN_FILES=\
 	test/test.cpp \
 	test/catch_main.cpp \
 	main/game_test.cpp \
+	main/network_test.cpp \
 	main/train.cpp
 
 TEST_EXECUTABLE=test
+NET_TEST_EXECUTABLE=nettest
 GAME_EXECUTABLE=game
 TRAIN_EXECUTABLE=train
 
@@ -38,15 +41,22 @@ EXECUTABLE_FILES  = $(EXECUTABLE_NAME:%=$(BIN)/%)
 OBJECT_FILES      = $(SOURCE_FILES:%.cpp=$(OBJ)/%.o)
 OBJECT_FILES_MAIN = $(MAIN_FILES:%.cpp=$(OBJ)/%.o)
 
-all: $(TEST_EXECUTABLE) $(GAME_EXECUTABLE) $(TRAIN_EXECUTABLE)
+all: $(TEST_EXECUTABLE) $(NET_TEST_EXECUTABLE) $(GAME_EXECUTABLE) $(TRAIN_EXECUTABLE)
 
 # Make executables
 $(TEST_EXECUTABLE): $(BIN)/$(TEST_EXECUTABLE)
+$(NET_TEST_EXECUTABLE): $(BIN)/$(NET_TEST_EXECUTABLE)
 $(GAME_EXECUTABLE): $(BIN)/$(GAME_EXECUTABLE)
 $(TRAIN_EXECUTABLE): $(BIN)/$(TRAIN_EXECUTABLE)
 
 # Link object files
 $(BIN)/$(TEST_EXECUTABLE): $(OBJ)/test/catch_main.o $(OBJECT_FILES) $(OBJ)/test/test.o
+	@echo Building $^
+	@$(CC) $(LDFLAGS) -o $@ $^
+	@echo "Build Successful"
+
+
+$(BIN)/$(NET_TEST_EXECUTABLE): $(OBJ)/main/network_test.o $(OBJECT_FILES)
 	@echo Building $^
 	@$(CC) $(LDFLAGS) -o $@ $^
 	@echo "Build Successful"
--- src/game/player.h
+++ src/game/player.h
@@ -8,7 +8,10 @@
 #ifndef CHECKERS_PLAYER_H
 #define CHECKERS_PLAYER_H
 
+#include "asknet4.h"
 #include "board_state.h"
+#include "jump_generator.h"
+#include <ctime>
 
 /* class Player
  *
@@ -38,8 +41,74 @@ public:
      */
     virtual BoardState takeMove(const BoardState & board) = 0;
 
-    virtual ~Player() = default; 
+    virtual ~Player() = default;
 
 };
+
+
+/* class UnPlayer
+ *
+ * Player implementation which forfeits the game. Useful as a placeholder.
+ */
+class UnPlayer : public Player {
+public:
+    UnPlayer(int color):Player(color) {}
+
+    BoardState takeMove(const BoardState & board) override;
+};
+
+
+/* class RandomPlayer
+ *
+ * Player implementation which chooses moves at random
+ */
+class RandomPlayer : public Player {
+private:
+    JumpGenerator gen;
+
+public:
+    RandomPlayer(int color):Player(color) {
+        std::srand(std::time(nullptr));
+    }
+
+    BoardState takeMove(const BoardState & board) override;
+};
+
+
+/* class PieceCountPlayer
+ *
+ * Player implementation which does a minimax search with piece count evaluation
+ * function. Default search depth is 4.
+ */
+class PieceCountPlayer : public Player {
+private:
+    size_t _depth = 4;
+
+public:
+    PieceCountPlayer (int color):Player(color) {}
+    PieceCountPlayer (int color, size_t depth):Player(color), _depth(depth) {}
+
+    BoardState takeMove(const BoardState & board) override;
+};
+
+
+/* class AIPlayer
+ *
+ * Player implementation which uses a minimax search with neural network as
+ * board evaluation function.
+ */
+class AIPlayer : public Player {
+private:
+    Network4 _net;
+    size_t _depth = 4;
+
+public:
+    AIPlayer (int color, const Network4 & net):Player(color), _net(net) {}
+    AIPlayer (int color, const Network4 & net, size_t depth):Player(color), _net(net), _depth(depth) {}
+
+    BoardState takeMove(const BoardState & board) override;
+    float evaluate(const BoardState & board, int player);
+};
+
 
 #endif
